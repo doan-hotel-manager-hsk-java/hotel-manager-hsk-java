a7d328b5ef5512c47f1442f61c63b4a21073c354
iamtun <letuan19431791.iuh@gmail>
2022-04-29 09:27:53 +0700

    custom demo push

--- src/gui/Login.java
+++ src/gui/Login.java
@@ -183,7 +183,7 @@ public class Login extends javax.swing.JFrame {
           String username = txtUserName.getText();
           String password = new String(txtPassword.getPassword());
           
-          //Phuong abc alo alo cc
+          //Phuong abc alo alo \dd
           if(username.equals("admin") && password.equals("admin")) {
             MainManager mainManager = new MainManager();
             mainManager.setLocationRelativeTo(this);
