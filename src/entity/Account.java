b862f6592b8532219aba39bde31932eb129a0b1f
VoMinhPhuong <vominhphuongk2@gmail.com>
2022-05-02 10:06:37 +0700

    new login

--- src/entity/Account.java
+++ src/entity/Account.java
@@ -20,4 +20,21 @@ public class Account {
         this.userName = userName;
         this.passWord = passWord;
     } 
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassWord() {
+        return passWord;
+    }
+
+    public void setPassWord(String passWord) {
+        this.passWord = passWord;
+    }
+    
 }
