4ef598759090b539f91bd044f8e2ae27c6f04545
iamtun <letuan19431791.iuh@gmail>
2022-05-03 09:36:31 +0700

    create method DAO room, room type, room status type

--- src/dao/RoomDAO.java
+++ src/dao/RoomDAO.java
@@ -4,10 +4,22 @@
  */
 package dao;
 
+import connection.DatabaseConnection;
+import entity.Room;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author vomin
  */
 public class RoomDAO {
-    
+   //tuan
+   private final String SELECT_ALL_ROOM = "SELECT * FROM PHONG";
+   private final String SELECT_ROOM_BY_NAME = "SELECT * FROM PHONG WHERE TENPHONG = ?";
+   
+
 }
--- src/dao/RoomTypeDAO.java
+++ src/dao/RoomTypeDAO.java
@@ -4,10 +4,71 @@
  */
 package dao;
 
+import connection.DatabaseConnection;
+import entity.RoomType;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.PreparedStatement;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author vomin
  */
 public class RoomTypeDAO {
+    //tuan
+    private final String MA_LP = "MALOAIPHONG";
+    private final String TEN_LP = "TenLOAIPHONG";
+    private final String DONGIA = "DONGIA";
     
+    private final String SELECT_ALL_ROOMTYPE = "SELECT * FROM LOAIPHONG";
+    private final String SELECT_ALL_ROOMTYPE_BY_ID = "SELECT * FROM WHERE MALOAIPHONG = ?";
+    
+    public List<RoomType> getAllRoomTypes() {
+        List<RoomType> roomTypeList = new ArrayList<>();
+        try(Connection conn = DatabaseConnection.opConnection();
+                PreparedStatement pstmt = conn.prepareStatement(SELECT_ALL_ROOMTYPE)) {
+            try(ResultSet rs = pstmt.executeQuery();) {
+                while(rs.next()) {
+                    String maLP = rs.getString(MA_LP);
+                    String tenLP = rs.getString(TEN_LP);
+                    Double donGia = rs.getDouble(DONGIA);
+                    
+                    RoomType roomType = new RoomType(maLP, tenLP, donGia);
+                    roomTypeList.add(roomType);
+                }
+                
+                return roomTypeList;
+            }
+        }catch(Exception e) {
+             System.err.println("getAllRoomType(): connect db fail");
+            e.printStackTrace();
+        }
+        
+        return null;
+    }
+    
+    public RoomType findRoomTypeById(String id) {
+        try(Connection conn = DatabaseConnection.opConnection();
+               PreparedStatement pstmt = conn.prepareStatement(SELECT_ALL_ROOMTYPE_BY_ID) ) {
+            pstmt.setString(1, id);
+            
+            try(ResultSet rs = pstmt.executeQuery()) {
+                if(rs.next()) {
+                    String maLP = rs.getString(MA_LP);
+                    String tenLP = rs.getString(TEN_LP);
+                    Double donGia = rs.getDouble(DONGIA);
+                    
+                    RoomType roomType = new RoomType(maLP, tenLP, donGia);
+                    return roomType;
+                }
+            }
+        }catch(Exception e) {
+             System.err.println("findRoomTypeById(): connect db fail");
+            e.printStackTrace();
+        }
+        
+        return null;
+    }
 }
